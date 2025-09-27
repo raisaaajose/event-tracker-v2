b6b9620c7771e0ad5968cbf9a5f2148dea57b0a2
JothishKamal <jothishkamal@gmail.com>
2025-09-28 02:56:27 +0530

    chore: revert api/auth.py

--- app/api/auth.py
+++ app/api/auth.py
@@ -195,7 +195,7 @@ async def google_callback(request: Request):
         request.session["user_id"] = user_id
 
         if frontend:
-            return RedirectResponse(url=f"{frontend}/home")
+            return RedirectResponse(url=f"{frontend}?login=success&user_id={user_id}")
 
         return JSONResponse(
             {
